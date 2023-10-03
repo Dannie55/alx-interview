04e7dd5ab4ce871996a2c6410f3ed153b433de3a
Dannie55 <ezirimdaniel080@gmail.com>
2023-10-03 19:40:14 +0000

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
 """Solution to Lockboxes problem"""
 
+
 def canUnlockAll(boxes):
     """Determines if all the boxes can be opened"""
-    if not isinstance(boxes, list) or len(boxes) == 0:
+    if (type(boxes) is not list or len(boxes) == 0):
         return False
-    
-    # Create a set to keep track of opened boxes
-    opened_boxes = {0}
-    
-    # Iterate through each box and its keys
-    for box_index, keys in enumerate(boxes):
-        if box_index in opened_boxes:
-            # Add keys to the set of opened boxes
-            opened_boxes.update(keys)
-    
-    # Check if all boxes have been opened
-    return len(opened_boxes) == len(boxes)
-
+    for k in range(1, len(boxes) - 1):
+        unlocked = False
+        for i in range(len(boxes)):
+            unlocked = k in boxes[i] and k != i
+            if unlocked:
+                break
+        if unlocked is False:
+            return unlocked
+    return True
